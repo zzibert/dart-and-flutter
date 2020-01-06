6895d8712a200282d49660037c25690a20f7b3a9
Žan Žiber <zan.zibert@ulu.io>
2020-01-06 12:49:27 +0100

    functions in array

--- main.dart
+++ main.dart
@@ -1,15 +1,9 @@
 int add(int x, int y) => x + y;
 int sub(int x, int y) => x - y;
 
-choose(bool op) {
-  if (op == true) {
-    return add;
-  } else {
-    return sub;
-  }
-}
+List operators = [add, sub];
 
 void main() {
-  var result = choose(true)(10, 20);
+  var result = operators[0](10, 20);
   print("Result is ${result}");
 }
