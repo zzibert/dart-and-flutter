6606c7cb2b45a1bc56fc0bb78d98c8b617228f35
Žan Žiber <zan.zibert@ulu.io>
2020-01-06 10:20:32 +0100

    lists

--- main.dart
+++ main.dart
@@ -6,4 +6,16 @@ void main() {
   String s = "${x + y}";
 
   print(s);
+
+  bool b = true;
+
+  print(b);
+
+  List l = [1, 2, 3];
+
+  print(l[0]);
+
+  List<String> ls = ["1", "2", "3"];
+
+  print(ls[1]);
 }
