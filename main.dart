280071f95ace03d484e1fedad0ae7a4dd1b6c6b7
Žan Žiber <zan.zibert@ulu.io>
2020-01-06 10:24:29 +0100

    maps

--- main.dart
+++ main.dart
@@ -1,21 +1,9 @@
 void main() {
-  int x = 10;
+  Map<String, int> map = {
+    'A': 10,
+    'B': 20,
+    'C': 30
+  };
 
-  double y = 10.0;
-
-  String s = "${x + y}";
-
-  print(s);
-
-  bool b = true;
-
-  print(b);
-
-  List l = [1, 2, 3];
-
-  print(l[0]);
-
-  List<String> ls = ["1", "2", "3"];
-
-  print(ls[1]);
+  print(map['B']);
 }
